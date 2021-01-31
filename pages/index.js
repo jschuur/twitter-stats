0c65b90eee6aebca5d0eefa1f7d58d3c3d9eed15
Joost Schuur <jschuur@jschuur.com>
2021-01-31 11:33:26 +0000

    Test showing followers on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import { connectToDatabase } from '../util/mongodb';
 
-export default function Home({ isConnected }) {
-  return <h1>Twitter Stats</h1>;
+export default function Home({ accounts }) {
+  return (
+    <>
+      <h1>Twitter Stats</h1>
+      {accounts.map((account) => (
+        <>
+          <h2>{account.screen_name}</h2>
+          <p>{Intl.NumberFormat().format(account.followers)} followers</p>
+        </>
+      ))}
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase()
+  const { db } = await connectToDatabase();
 
-  const isConnected = await client.isConnected()
+  const accounts = await db.collection('accounts').find({}).toArray();
 
   return {
-    props: { isConnected },
-  }
+    props: {
+      accounts: JSON.parse(JSON.stringify(accounts)),
+    },
+  };
 }
