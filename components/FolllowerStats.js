31dbaf858a1064c315a0923705954466a1447a8d
Joost Schuur <jschuur@jschuur.com>
2021-02-03 22:19:55 +0000

    Refactored FollowerStats

--- components/FolllowerStats.js
+++ components/FolllowerStats.js
@@ -1,15 +1,14 @@
 import { RECENT_DAYS } from '../util/config';
 
-function FollowerStatsAccount(account) {
+function buildAccountElements(account) {
+  var followerChangeText = '-',
+    followerChangeColor = '';
+
   const {
-    screen_name,
     recent_followers_count,
     profile: { followers_count },
   } = account;
-  const followers = Intl.NumberFormat('en-GB').format(followers_count);
   const followerChange = followers_count - recent_followers_count;
-  var followerChangeText = '-',
-    followerChangeColor = '';
 
   if (followerChange != 0) {
     followerChangeColor = followerChange > 0 ? 'text-green-500' : 'text-red-500';
@@ -19,9 +18,20 @@ function FollowerStatsAccount(account) {
     }).format(followerChange);
   }
 
+  return {
+    followerCount: Intl.NumberFormat('en-GB').format(followers_count),
+    followerChangeColor,
+    followerChangeText,
+  };
+}
+
+function FollowerStatsAccount(account) {
+  const { screen_name } = account;
+  const { followerCount, followerChangeColor, followerChangeText } = buildAccountElements(account);
+
   return (
     <div className='mt-2' key={screen_name}>
-      <a href={`https://twitter.com/${screen_name}`}>{screen_name}</a>: {followers} (
+      <a href={`https://twitter.com/${screen_name}`}>{screen_name}</a>: {followerCount} (
       <span className={followerChangeColor}>{followerChangeText}</span>)
     </div>
   );
@@ -30,9 +40,11 @@ function FollowerStatsAccount(account) {
 export default function FollowerStats({ accounts }) {
   return (
     <div>
-    <h1 className="text-4xl">Twitter Followers</h1>
+      <h1 className='text-4xl'>Twitter Followers</h1>
       {accounts.map(FollowerStatsAccount)}
-      <p className="text-xs pt-3 float-right"><i>changes in the past {RECENT_DAYS} days</i></p>
+      <p className='text-xs pt-3 float-right'>
+        <i>changes in the past {RECENT_DAYS} days</i>
+      </p>
     </div>
-  )
+  );
 }
